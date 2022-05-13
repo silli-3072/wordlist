9619ef83881869c05daf527926f2977e0d394acb
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-05-13 15:58:34 +0900

    Initial Commit

---
+++ wordlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordlist
+//
+//  Created by 春田実利 on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
